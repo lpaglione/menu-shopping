11393480fa304fb5b7443cef792346e34d034304
lpaglione <l.paglione@orcid.org>
2016-11-24 20:02:13 -0500

    Adding in Cloudboost & getter/setters

--- MSIngredient.js
+++ MSIngredient.js
@@ -6,16 +6,72 @@
 // Ingredient class         
 // --------------------------------------
 
+/* Initialize CloudBoost */
+CB.CloudApp.init('fhioibnbvdrp', '4bb21293-3165-4d27-9b74-e173bf153fa8');
+
 class MSIngredient {
-	constructor(name, description) {
-		this.name = name; //name of the ingredient
-		this.description = description; //description of the ingredient
+	constructor(name, description, cloudObj) {
+		this._cloudObj = cloudObj;
 		this.image = "http://imgc.allpostersimages.com/images/P-473-488-90/75/7570/I9JD300Z/posters/marilyna-large-health-food-selection-in-white-porcelain-bowls-and-dishes-over-papyrus-background.jpg"
 		this.purchaseUnits = "oz"; //the units used when purchasing this item, default=ounces
-		this.storeLocation; //the location within the store to find this item, default=undefined
+		this.storeLocation = ""; //the location within the store to find this item, default=undefined
 		this.meals = []; //the list of meals that this ingredient is in, default=undefined
+
+		//special getter/setter
+		this.name = name; //name of the ingredient
+		this.description = description; //description of the ingredient
 	}
 
+	// **************************************
+	// Getters & setters - NAME
+	// **************************************
+	get name() {
+		return this._cloudObj.get("name");
+	}
+
+	set name(name) {
+		this._name = name;
+		this._cloudObj.set("name", name);
+		console.log("I'm setting the name!")
+	}
+
+	// **************************************
+	// Getters & setters - DESCRIPTION
+	// **************************************
+	get description() {
+		return this._cloudObj.get("description");
+	}
+
+	set description(description) {
+		this._description = description;
+		this._cloudObj.set("description", description);
+	}
+
+	// **************************************
+	// CLOUDBOOST - save & delete object
+	// **************************************
+	save() {
+		this._cloudObj.save({
+			succes: (obj) => {},
+			error: (err) => {
+				console.log(err);
+			}
+		});
+	}
+
+	delete() {
+		this._cloudObj.delete({
+			success: (obj) => {},
+			error: (err) => {
+				console.log(err);
+				$(".error").html(err);
+			}
+		});
+	}
+
+	// **************************************
+	// Other object functions
+	// **************************************
 	inPurchaseUnits(amount, units) {
 		//converts the amount to the equivalent amount in the purchase units for the ingredient
 	}
--- menu-shopping.js
+++ menu-shopping.js
@@ -8,30 +8,81 @@
 
 //import * as ingredient from 'MSIngredient';
 
+/* Initialize CloudBoost */
+CB.CloudApp.init('fhioibnbvdrp', '4bb21293-3165-4d27-9b74-e173bf153fa8');
+
 class MenuShopping {
-	constructor(tableIngredients) {
+	constructor() {
 		this.appIngredients = [];
-		this.tableIngredients = tableIngredients; // the table ID for ingredients
+		//this.tableIngredients = tableIngredients;
+	}
+
+	initIngredientsPage(tableIngredients) {
+		this.listIngredients(tableIngredients);
 	}
 
 	addIngredient(name, description) {
 		console.log("In add ingredient function");
 		console.log($(name).val());
-		this.appIngredients.push(new MSIngredient($(name).val(), $(description).val()));
+
+		// Create a new incredient object and save it to the database
+
+		const ingredientCloudObj = new CB.CloudObject("MSIngredient");
+		const ingredientObj = new MSIngredient($(name).val(), $(description).val(), ingredientCloudObj);
+		ingredientObj.save();
+
+		// Add it to the active appIngredents list
+		this.appIngredients.push(ingredientObj);
+
+		//this.appIngredients.push(new MSIngredient($(name).val(), $(description).val()));
 		console.log(this.appIngredients);
-		this.listIngredients();
 	}
 
-	listIngredients() { 
+	listIngredients(tableIngredients) { 
 		console.log("in the list");
 		//clear the table and replace the headers
-		$(this.tableIngredients+" tbody").remove();
-		$(this.tableIngredients).append(`<tr><th>Name</th><th>Description</th></tr>`)
+		$(tableIngredients+" tbody").remove();
+		$(tableIngredients).append(`<tr><th>Name</th><th>Description</th></tr>`)
 
 		for (let z=0; z<this.appIngredients.length; z++) {
 			console.log(this.appIngredients[z].name);
-			$(this.tableIngredients).append(`<tr><td>${this.appIngredients[z].name}</td><td>${this.appIngredients[z].description}</td></tr>`)
+			$(tableIngredients).append(`<tr><td>${this.appIngredients[z].name}</td><td>${this.appIngredients[z].description}</td></tr>`)
 		}
 
 	}
 }
+
+/* COOKIES! */
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+/*function checkCookie() {
+    var username=getCookie("username");
+    if (username!="") {
+        alert("Welcome again " + username);
+    } else {
+        username = prompt("Please enter your name:", "");
+        if (username != "" && username != null) {
+            setCookie("username", username, 365);
+        }
+    }
+}*/
