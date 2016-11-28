4b1a264c671de148bb49f1441ab5e41788c2a462
lpaglione <l.paglione@orcid.org>
2016-11-28 08:52:28 -0500

    Changed indentation to the NHA standard 2 spaces
    Was 4-space tabs. Now 2 spaces (not tabs)

--- MSIngredient.js
+++ MSIngredient.js
@@ -1,8 +1,8 @@
 // --------------------------------------
 // Laura Paglione (MENTOR)            
-// NanoHacker Academy	
+// NanoHacker Academy 
 // Fall 2016: Object oriented
-// November 23, 2016	
+// November 23, 2016  
 // Ingredient class         
 // --------------------------------------
 
@@ -10,98 +10,98 @@
 CB.CloudApp.init('fhioibnbvdrp', '4bb21293-3165-4d27-9b74-e173bf153fa8');
 
 class MSIngredient {
-	constructor(name, description, image, cloudObj) {
-		// TO DO: refactor functions so that all attributes are set via the cloudObj to remove redundancy
-		this._cloudObj = cloudObj;
-		this.purchaseUnits = "oz"; //the units used when purchasing this item, default=ounces
-		this.storeLocation = ""; //the location within the store to find this item, default=undefined
-		this.meals = []; //the list of meals that this ingredient is in, default=undefined
+  constructor(name, description, image, cloudObj) {
+    // TO DO: refactor functions so that all attributes are set via the cloudObj to remove redundancy
+    this._cloudObj = cloudObj;
+    this.purchaseUnits = "oz"; //the units used when purchasing this item, default=ounces
+    this.storeLocation = ""; //the location within the store to find this item, default=undefined
+    this.meals = []; //the list of meals that this ingredient is in, default=undefined
 
-		//special getter/setter
-		this.name = name; //name of the ingredient
-		this.description = description; //description of the ingredient
-		this.image = image; // the image for the ingredient
-	}
+    //special getter/setter
+    this.name = name; //name of the ingredient
+    this.description = description; //description of the ingredient
+    this.image = image; // the image for the ingredient
+  }
 
-	// **************************************
-	// Getters & setters - NAME
-	// **************************************
-	get name() {
-		return this._cloudObj.get("name");
-	}
+  // **************************************
+  // Getters & setters - NAME
+  // **************************************
+  get name() {
+    return this._cloudObj.get("name");
+  }
 
-	set name(name) {
-		this._name = name;
-		this._cloudObj.set("name", name);
-	}
+  set name(name) {
+    this._name = name;
+    this._cloudObj.set("name", name);
+  }
 
-	// **************************************
-	// Getters & setters - DESCRIPTION
-	// **************************************
-	get description() {
-		return this._cloudObj.get("description");
-	}
+  // **************************************
+  // Getters & setters - DESCRIPTION
+  // **************************************
+  get description() {
+    return this._cloudObj.get("description");
+  }
 
-	set description(description) {
-		this._description = description;
-		this._cloudObj.set("description", description);
-	}
+  set description(description) {
+    this._description = description;
+    this._cloudObj.set("description", description);
+  }
 
-	// **************************************
-	// Getters & setters - IMAGE
-	// **************************************
-	get image() {
-		return this._cloudObj.get("image");
-	}
+  // **************************************
+  // Getters & setters - IMAGE
+  // **************************************
+  get image() {
+    return this._cloudObj.get("image");
+  }
 
-	set image(image) {
-		const defaultImage = "http://imgc.allpostersimages.com/images/P-473-488-90/75/7570/I9JD300Z/posters/marilyna-large-health-food-selection-in-white-porcelain-bowls-and-dishes-over-papyrus-background.jpg"
+  set image(image) {
+    const defaultImage = "http://imgc.allpostersimages.com/images/P-473-488-90/75/7570/I9JD300Z/posters/marilyna-large-health-food-selection-in-white-porcelain-bowls-and-dishes-over-papyrus-background.jpg"
 
-		if(image =="") image = defaultImage; //set to the default image if blank
+    if(image =="") image = defaultImage; //set to the default image if blank
 
-		this._image = image;
-		this._cloudObj.set("image", image);
-	}
+    this._image = image;
+    this._cloudObj.set("image", image);
+  }
 
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// FUNCTION: ingredientDisplayAsMedia
-	// Append to the HTML at the location passed to display the 
-	// ingredient as a bootstrap media object
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	ingredientDisplayAsMedia(locationID){
-		$(locationID).append(`<div class='media'><div class='media-left media-top'><a href='#' data-toggle="tooltip" title="View ingredient"><div style='width: 64px'><img class='media-object img-rounded center-block' src=${this.image} alt='image' style='max-width: 64px; max-height: 64px;'></div></a></div><div class='media-body'><h4 class='media-heading'>${this.name}</h4><button type='button' class='btn btn-default btn-sm pull-right' data-toggle='tooltip' title='Edit this ingredient'><span class='glyphicon glyphicon glyphicon-edit' aria-hidden='true'></span> Edit</button><p>${this.description}<br/></p><hr/></div></div>`)
-	}
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  // FUNCTION: ingredientDisplayAsMedia
+  // Append to the HTML at the location passed to display the 
+  // ingredient as a bootstrap media object
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  ingredientDisplayAsMedia(locationID){
+    $(locationID).append(`<div class='media'><div class='media-left media-top'><a href='#' data-toggle="tooltip" title="View ingredient"><div style='width: 64px'><img class='media-object img-rounded center-block' src=${this.image} alt='image' style='max-width: 64px; max-height: 64px;'></div></a></div><div class='media-body'><h4 class='media-heading'>${this.name}</h4><button type='button' class='btn btn-default btn-sm pull-right' data-toggle='tooltip' title='Edit this ingredient'><span class='glyphicon glyphicon glyphicon-edit' aria-hidden='true'></span> Edit</button><p>${this.description}<br/></p><hr/></div></div>`)
+  }
 
-	// **************************************
-	// CLOUDBOOST - save & delete object
-	// **************************************
-	save() {
-		this._cloudObj.save({
-			success: (obj) => {
-				console.log("saved ingredient: " + this._cloudObj.get("name"))
-			},
-			error: (err) => {
-				console.log(err);
-			}
-		});
-	}
+  // **************************************
+  // CLOUDBOOST - save & delete object
+  // **************************************
+  save() {
+    this._cloudObj.save({
+      success: (obj) => {
+        console.log("saved ingredient: " + this._cloudObj.get("name"))
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
 
-	delete() {
-		this._cloudObj.delete({
-			success: (obj) => {},
-			error: (err) => {
-				console.log(err);
-				$(".error").html(err);
-			}
-		});
-	}
+  delete() {
+    this._cloudObj.delete({
+      success: (obj) => {},
+      error: (err) => {
+        console.log(err);
+        $(".error").html(err);
+      }
+    });
+  }
 
-	// **************************************
-	// Other object functions
-	// **************************************
-	inPurchaseUnits(amount, units) {
-		//converts the amount to the equivalent amount in the purchase units for the ingredient
-	}
+  // **************************************
+  // Other object functions
+  // **************************************
+  inPurchaseUnits(amount, units) {
+    //converts the amount to the equivalent amount in the purchase units for the ingredient
+  }
 }
 
 // export { MSIngredient };
--- menu-shopping.js
+++ menu-shopping.js
@@ -1,8 +1,8 @@
 // --------------------------------------
 // Laura Paglione (MENTOR)            
-// NanoHacker Academy	
+// NanoHacker Academy 
 // Fall 2016: Object oriented
-// November 23, 2016	
+// November 23, 2016  
 // MenuShopping application                  
 // --------------------------------------
 
@@ -12,98 +12,98 @@
 CB.CloudApp.init('fhioibnbvdrp', '4bb21293-3165-4d27-9b74-e173bf153fa8');
 
 class MenuShopping {
-	constructor() {
-		this.appIngredients = []
-		this.getAllDBIngredients();
-	}
+  constructor() {
+    this.appIngredients = []
+    this.getAllDBIngredients();
+  }
 
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// FUNCTION: addIngredient
-	// Adds a single ingredient to the appIngredient list, the 
-	// database, and to the display on the ingredient list page
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	addIngredient(name, description, image) {
-		// Check if the ingredient already exists
-		let query = new CB.CloudQuery("MSIngredient");
-		query.equalTo('name', name); //is the name already in the database?
-		query.find({
-			success: (list) => {
-				if (list.length > 0) {
-					// The item is already in the list - alert the user
-					alert(name+" already exists in the Available ingredients list.");
-					// TO DO: Make this a Boostrap alert
-				} else {
-					//item doesn't already exist. Add it!
-					// Create a new ingredient object and save it to the database
-					let ingredientCloudObj = new CB.CloudObject("MSIngredient");
-					let ingredientObj = new MSIngredient(name, description, image, ingredientCloudObj);
-					ingredientObj.save();
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  // FUNCTION: addIngredient
+  // Adds a single ingredient to the appIngredient list, the 
+  // database, and to the display on the ingredient list page
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  addIngredient(name, description, image) {
+    // Check if the ingredient already exists
+    let query = new CB.CloudQuery("MSIngredient");
+    query.equalTo('name', name); //is the name already in the database?
+    query.find({
+      success: (list) => {
+        if (list.length > 0) {
+          // The item is already in the list - alert the user
+          alert(name+" already exists in the Available ingredients list.");
+          // TO DO: Make this a Boostrap alert
+        } else {
+          //item doesn't already exist. Add it!
+          // Create a new ingredient object and save it to the database
+          let ingredientCloudObj = new CB.CloudObject("MSIngredient");
+          let ingredientObj = new MSIngredient(name, description, image, ingredientCloudObj);
+          ingredientObj.save();
 
-					// Add it to the active appIngredients list
-					this.appIngredients.push(ingredientObj);
+          // Add it to the active appIngredients list
+          this.appIngredients.push(ingredientObj);
 
-					console.log(this.appIngredients);
+          console.log(this.appIngredients);
 
-					// Add the item to the already-displayed list
-					ingredientObj.ingredientDisplayAsMedia("#ingredientList");
-				}
-				
-			},
-			error: (err) => {
-				console.log(err);
-			}
-		});
-	}
+          // Add the item to the already-displayed list
+          ingredientObj.ingredientDisplayAsMedia("#ingredientList");
+        }
+        
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
 
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// FUNCTION: removeIngredient
-	// Removes a single ingredient from the appIngredient list, the 
-	// database, and the display on the ingredient list page
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// TO DO: add this function
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  // FUNCTION: removeIngredient
+  // Removes a single ingredient from the appIngredient list, the 
+  // database, and the display on the ingredient list page
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  // TO DO: add this function
 
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// FUNCTION: editIngredient
-	// Edit an ingredient on the appIngredient list, the 
-	// database, and the display on the ingredient list page
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// TO DO: add this function
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  // FUNCTION: editIngredient
+  // Edit an ingredient on the appIngredient list, the 
+  // database, and the display on the ingredient list page
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  // TO DO: add this function
 
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// FUNCTION: displayList
-	// Displays all of the ingredients in the list as Bootstrap
-	// media objects to location identified by ID #ingredientList
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	displayList() {
-		// Now display the ingredient list in the table with the ID #ingredientList
-		// clear the list
-		$("#ingredientList").html("");
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  // FUNCTION: displayList
+  // Displays all of the ingredients in the list as Bootstrap
+  // media objects to location identified by ID #ingredientList
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  displayList() {
+    // Now display the ingredient list in the table with the ID #ingredientList
+    // clear the list
+    $("#ingredientList").html("");
 
-		// and list the ingredients as Bootstrap mediea as defined by the MSIngredients object
-		for (let z=0; z<this.appIngredients.length; z++) {
-			this.appIngredients[z].ingredientDisplayAsMedia("#ingredientList")
-		}
-	}
+    // and list the ingredients as Bootstrap mediea as defined by the MSIngredients object
+    for (let z=0; z<this.appIngredients.length; z++) {
+      this.appIngredients[z].ingredientDisplayAsMedia("#ingredientList")
+    }
+  }
 
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	// FUNCTION: getAllDBIngredients
-	// Gets all of the ingredients stored in the database and stores
-	// them in this application object
-	// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-	getAllDBIngredients() {
-		const query = new CB.CloudQuery("MSIngredient");
-		query.exists('name');
-		query.find({
-			success: (list) => {
-				// iterate through the list to add the items from the database to the object
-				this.appIngredients = list.map((obj) => {
-					return new MSIngredient(obj.get("name"), obj.get("description"), obj.get("image"), obj)
-				});
-				this.displayList("#ingredientList");
-			},
-			error: function(err) {
-				console.log(err);
-			}
-		})
-	}
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  // FUNCTION: getAllDBIngredients
+  // Gets all of the ingredients stored in the database and stores
+  // them in this application object
+  // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  getAllDBIngredients() {
+    const query = new CB.CloudQuery("MSIngredient");
+    query.exists('name');
+    query.find({
+      success: (list) => {
+        // iterate through the list to add the items from the database to the object
+        this.appIngredients = list.map((obj) => {
+          return new MSIngredient(obj.get("name"), obj.get("description"), obj.get("image"), obj)
+        });
+        this.displayList("#ingredientList");
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    })
+  }
 }
